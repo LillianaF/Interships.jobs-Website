3db18b869d2a7234b6f95ee6eed3b30edd689681
Glitch (hello-webpage) <none>
2020-07-29 02:05:52 +0000

    📊📠 Checkpoint ./index.html:19921051/18794 ./script.js:19921051/1832 ./style.css:19921051/2984

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+function dropDown() {
+  var x = document.getElementsByClassName('expand');
+  if (x.style.display == 'none') {
+    x.style.display = 'block';
+  } else{
+    x.style.display = 'none';
+  }
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function postToGoogle() {
+                var field1 = $("#name").val();
+                var field2 = $("#description").val();
+                var field3 = $("#person").val();
+                var field4 = $("#contact").val();
+
+				if(field1 == ""){
+					alert('Please Fill In Your Company Name');
+					document.getElementById("name").focus();
+					return false;
+				}
+				if(field2 == ""){
+					alert('Please Describe Your Company');
+					document.getElementById("description").focus();
+					return false;
+				}
+				if(field3 == ""){
+					alert('Please Fill In Your Contact Name');
+					document.getElementById("person").focus();
+					return false;
+				}
+        if(field4 == ""){
+					alert('Please Fill In Your Contact Email');
+					document.getElementById("contact").focus();
+					return false;
+				}
+
+                $.ajax({
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSfFRDBlJv4Dz7x3w9vL477Izq7D7f_eapi1-TVCXh86Bh3fzw/formResponse?",
+					data: {"entry.151408002": field1, "entry.519686267": field2, "entry.258138801": field3, "entry.1693006279": field4},
+                    type: "POST",
+                    dataType: "xml",
+                    success: function(d)
+					{
+					},
+					error: function(x, y, z)
+						{
+							/*$('#success-msg').show();
+							$('#form').hide();*/
+              alert('Your Response has been Recorded');
+              document.getElementById('form').reset();
+
+						}
+                });
+				return false;
+            }
+
